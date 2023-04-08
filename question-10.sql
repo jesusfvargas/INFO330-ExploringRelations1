347007fdec68e89823aee6293daaee58bd46a292
Jesus Vargas <jfv97@uw.edu>
2023-04-07 22:35:26 -0700

    2nd commit
    getting error 5, working on it

---
+++ question-10.sql
@@ -0,0 +1,2 @@
+INSERT INTO persons (first_name, last_name, age, gender, title, email, city, state, address)
+VALUES ('Jesus', 'Vargas', 26, 'Male', 'Mr', 'jfv97@uw.edu', 'Los Angeles', 'CA', 'Lebron Way');
